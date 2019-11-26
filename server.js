e12dadc135d115c281f27dcf68ee00dbb0f61da6
tejasnsuvarna <tejasnareshsuvarna@gmail.com>
2019-11-26 21:09:02 +0530

    Added background images for repair and cleaning. Updated server.js by addig modules.

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-// ////////#### IMPORTS ####////////
+////////#### IMPORTS ####////////
 
-// const express = require('express');
-// const hbs = require('express-handlebars');
-// //const bodyParser = require('body-parser');
-// // const fetch = require('node-fetch');
-// const app = express();
+const express = require('express');
+const hbs = require('express-handlebars');
+//const bodyParser = require('body-parser');
+// const fetch = require('node-fetch');
+const app = express();
 
-// ////////////////////////////////
-// ////////////////////////////////
+////////////////////////////////
+////////////////////////////////
 
 
 
-// ////////#### SETTING UP PORTS ####////////
+////////#### SETTING UP PORTS ####////////
 
-// const port = process.env.PORT || 3001;
-// app.listen(port, () => {
-//     console.log(`Server up on port ${port}`);
-// });
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`SweepWell Server up on port ${port}`);
+});
 
-// ////////////////////////////////
-// ////////////////////////////////
-
-console.log('Tejas');
+////////////////////////////////
+////////////////////////////////
