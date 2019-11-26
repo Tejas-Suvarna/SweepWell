5df1e4f2c919a70fd40c0126fbc00f745e27f6cd
tejasnsuvarna <tejasnareshsuvarna@gmail.com>
2019-11-26 20:19:45 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// ////////#### IMPORTS ####////////
+
+// const express = require('express');
+// const hbs = require('express-handlebars');
+// //const bodyParser = require('body-parser');
+// // const fetch = require('node-fetch');
+// const app = express();
+
+// ////////////////////////////////
+// ////////////////////////////////
+
+
+
+// ////////#### SETTING UP PORTS ####////////
+
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//     console.log(`Server up on port ${port}`);
+// });
+
+// ////////////////////////////////
+// ////////////////////////////////
+
+console.log('Tejas');
