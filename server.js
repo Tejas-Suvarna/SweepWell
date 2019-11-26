4919946e9ead6b33c9728ae8729633d742e310b6
tejasnsuvarna <tejasnareshsuvarna@gmail.com>
2019-11-26 21:51:26 +0530

    Added HBS Setup and Routes in server.js with correction of links. We have also moved the style.css and images into the public folder. We also created the HBS layouts adding home and about hbs files.

--- server.js
+++ server.js
@@ -10,6 +10,44 @@ const app = express();
 ////////////////////////////////
 
 
+////////#### HBS SETUP ####////////
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+
+////////////////////////////////
+////////////////////////////////
+
+
+////////#### FOR LINKS ####////////
+
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/images", express.static(__dirname + '/public/images'));
+
+////////////////////////////////
+////////////////////////////////
+
+
+////////#### CHECKING FOR ROUTES ####////////
+
+// Home Page
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// About Page
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+////////////////////////////////
+////////////////////////////////
+
 
 ////////#### SETTING UP PORTS ####////////
 
